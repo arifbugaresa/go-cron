26b8ae970fe2583afd2e0f3e7e92052bbfaf72c8
arifbugaresa <arifyuniartofajarbugaresa@gmail.com>
2022-06-27 14:26:28 +0700

    go cron menggunakan github.com/robfig/cron

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-cron
 
 go 1.17
+
+require github.com/robfig/cron v1.2.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
-func main()  {
-	
+import (
+	"fmt"
+	"github.com/robfig/cron"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+
+	// set scheduler berdasarkan zona waktu
+	jakartaTime, _ := time.LoadLocation("Asia/Jakarta")
+	scheduler := cron.NewWithLocation(jakartaTime)
+
+	// defer untuk stop scheduler sebelum fungsi berakhir
+	defer scheduler.Stop()
+
+	// set task yang akan dijalankan di scheduler
+	scheduler.AddFunc("0 */1 * * * *", func() {
+		SendAutoEveryMinute("Hi.....")
+	})
+
+	// set task yang akan dijalankan di scheduler
+	scheduler.AddFunc("* */1 * * * *", SendAutoEverySecond)
+
+	// start scheduller
+	scheduler.Start()
+
+	// trap SIGINT untuk trigger shutdown.
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
+
+}
+
+func SendAutoEveryMinute(mail string) {
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05") + mail)
+}
+
+func SendAutoEverySecond()  {
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05") + "SendAutoEverySecond")
 }
